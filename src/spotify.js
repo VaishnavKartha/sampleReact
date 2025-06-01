ecf008c637f490f70854942ff691703d19475e78
VaishnavKartha <146973489+VaishnavKartha@users.noreply.github.com>
2025-06-01 14:59:59 +0530

    Update spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 const authEndpoint="https://accounts.spotify.com/authorize?"
 const clientID="cd4bbbd43c9c4c93bd17029c58238aa0"
 const clientSecret="2ca606fd46ba4a74857ff085a9be664d";
-const redirectUrl="https://www.google.com/"
+const redirectUrl="https://vaishnavkartha.github.io/sampleReact/"
 const scopes=["user-library-read","playlist-read-private"];
 
 export const loginEndpoint=`${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
